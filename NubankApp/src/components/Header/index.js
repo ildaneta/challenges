5f534ef7107d7bae2fa43f80b9dc8b3d018226ab
ildasilva <ildaemanoely@hotmail.com>
2020-04-02 12:15:42 -0300

    feat: Creating the Header component with your styles

---
+++ NubankApp/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Container, Top, Logo, Title } from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import logo from '../../assets/Nubank_Logo.png';
+
+export default function Header() {
+  return (
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>Ilda</Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#fff" />
+    </Container>
+  );
+}
