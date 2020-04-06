3ddc774fc9385b2edbfc9e0f57454bb9aeb4ca2f
ildasilva <ildaemanoely@hotmail.com>
2020-04-06 13:14:56 -0300

    feat: Add the Card Menu in the Main component

--- NubankApp/src/pages/Main/index.js
+++ NubankApp/src/pages/Main/index.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
+import Menu from '../../components/Menu';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 453.698,65</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 26,34 recebida de Ilda Neta hoje às 17:46h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
--- NubankApp/src/pages/Main/styles.js
+++ NubankApp/src/pages/Main/styles.js
@@ -1,8 +1,67 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View`
+export const Container = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})`
   flex: 1;
   background: #8b10ae;
   padding-top: ${getStatusBarHeight()}px;
+  /* justify-content: center; */
+  padding-right: 20px;
+  padding-left: 20px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  max-height: 500px;
+  /* Vai fazer com que esse componente passe por cima dos demais */
+  z-index: 5;
+`;
+
+export const Card = styled.View`
+  flex: 1;
+  background: #fff;
+  border-radius: 4px;
+  height: 100%;
+
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0px;
+`;
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 30px;
+`;
+
+export const CardContent = styled.View`
+  flex: 1;
+  padding: 0 30px;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 13px;
+  color: #999;
+`;
+
+export const Description = styled.Text`
+  font-size: 32px;
+  margin-top: 3px;
+  color: #333;
+`;
+
+export const CardFooter = styled.View`
+  padding: 20px;
+  background: #eee;
+  border-radius: 4px;
+`;
+
+export const Annotation = styled.Text`
+  font-size: 12px;
+  color: #333;
 `;
