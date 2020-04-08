3ab382199118ac6e41c1cc28e7f61a055451ceb0
ildasilva <ildaemanoely@hotmail.com>
2020-04-08 18:00:17 -0300

    feat: Adding new components and styles

--- NubankApp/src/components/Menu/styles.js
+++ NubankApp/src/components/Menu/styles.js
@@ -4,26 +4,26 @@ import styled from 'styled-components/native';
 export const Container = styled(Animated.ScrollView)`
   margin: 0 auto;
   width: 100%;
-  max-height: 500px;
+  max-height: 550px;
 `;
 
 export const Code = styled.View`
   overflow: hidden;
   align-self: center;
-  padding: 10px;
+  padding: 5px;
   background: #fff;
 `;
 
 export const Nav = styled.View`
-  margin-top: 30px;
+  margin-top: 15px;
   border-top-width: ${StyleSheet.hairlineWidth}px;
   border-top-color: rgba(255, 255, 255, 0.8);
 `;
 
 export const NavItem = styled.View`
-  flex-direction: row;
+  flex-direction: ${(props) => (props.column ? 'column' : 'row')};
   align-items: center;
-  padding: 12px 0;
+  padding: 11px 0;
   border-bottom-width: ${StyleSheet.hairlineWidth}px;
   border-bottom-color: rgba(255, 255, 255, 0.8);
 `;
@@ -32,6 +32,19 @@ export const NavText = styled.Text`
   font-size: 15px;
   color: #fff;
   margin-left: 20px;
+  margin-right: 100px;
+  width: 160px;
+`;
+
+export const NavTextSmall = styled.Text`
+  font-size: 12px;
+  padding-left: 20px;
+  color: #abaaba;
+`;
+
+export const Teste = styled.View`
+  padding: 0;
+  margin: 0;
 `;
 
 export const SignOutButton = styled.TouchableOpacity`
@@ -49,4 +62,18 @@ export const SignOutButtonText = styled.Text`
   color: #fff;
   font-weight: bold;
   font-size: 13px;
+`;
+
+export const TitlesMenu = styled.Text`
+  font-size: 14px;
+  color: #fff;
+`;
+
+export const TitleMenuStrong = styled.Text`
+  font-weight: bold;
+`;
+
+export const WrapperTitlesMenu = styled.View`
+  align-items: center;
+  margin-top: 10px;
 `;
