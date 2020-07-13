7a29ba08416eb821a3356dc1cc1dc9f89d8e17c0
Ilda Neta <iildaemanoely@gmail.com>
2020-07-12 21:38:34 -0300

    feat: creating a new  mobile project

---
+++ financialApp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header/index';
+import Main from './components/Main/index';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
