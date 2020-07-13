dc18d121cf0c69f9450c2b998648632c4397fcb7
Ilda Neta <iildaemanoely@gmail.com>
2020-07-12 21:35:21 -0300

    feat: creating the Main component

---
+++ financialApp/src/components/Main/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {Container} from './styles';
+
+function Main() {
+  return <Container />;
+}
+
+export default Main;
---
+++ financialApp/src/components/Main/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  /* background-color: #bb7493; */
+  flex: 1;
+`;
