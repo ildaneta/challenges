8ddfc4364c8ef3280d3dcceac60195ca79c88b55
Ilda Neta <iildaemanoely@gmail.com>
2020-07-14 08:12:27 -0300

    feat: creating the main component and your styles

--- financialApp/src/components/Main/index.js
+++ financialApp/src/components/Main/index.js
@@ -1,16 +1,109 @@
 import React from 'react';
+import {TouchableOpacity, View, StyleSheet} from 'react-native';
 
-import {Container, ContainerTransactionsText, TransactionsText} from './styles';
+// import styles from './';
+
+import income from '../../assets/images/income.png';
+import expense from '../../assets/images/expanse.png';
+import grocery from '../../assets/images/grocery.png';
+import car from '../../assets/images/car.png';
+
+import {
+  Container,
+  ContainerTransactionsText,
+  TransactionsText,
+  Buttons,
+  ButtonsIcons,
+  ButtonsContainer,
+  WrapperButton,
+  TitleDays,
+  SafeAreaContainer,
+  ContainerOutgoing,
+  ContainerTitleOutgoing,
+  ContainerSubTitleOutgoing,
+  TitleOutgoing,
+  SubTitleOutgoing,
+  WrapperOutgoing,
+  ContainerImage,
+  Image,
+} from './styles';
 
 function Main() {
   return (
     <Container>
       <ContainerTransactionsText>
         <TransactionsText extraBold>Transações Recentes</TransactionsText>
-        <TransactionsText>Veja todas</TransactionsText>
+        <TouchableOpacity>
+          <TransactionsText>Veja todas</TransactionsText>
+        </TouchableOpacity>
       </ContainerTransactionsText>
+
+      <View>
+        <ButtonsContainer>
+          <WrapperButton first noImage style={style.shadow}>
+            <Buttons>Todos</Buttons>
+          </WrapperButton>
+
+          <WrapperButton style={style.shadow}>
+            <ButtonsIcons source={income} />
+            <Buttons>Entradas</Buttons>
+          </WrapperButton>
+
+          <WrapperButton style={style.shadow}>
+            <ButtonsIcons source={expense} />
+            <Buttons>Saídas</Buttons>
+          </WrapperButton>
+        </ButtonsContainer>
+      </View>
+
+      <SafeAreaContainer>
+        <TitleDays>TODAY</TitleDays>
+
+        <WrapperOutgoing first>
+          <ContainerImage>
+            <Image source={grocery} />
+          </ContainerImage>
+
+          <ContainerOutgoing>
+            <ContainerTitleOutgoing>
+              <TitleOutgoing>Padaria</TitleOutgoing>
+              <TitleOutgoing>- R$ 50,58</TitleOutgoing>
+            </ContainerTitleOutgoing>
+
+            <ContainerSubTitleOutgoing>
+              <SubTitleOutgoing>Super pão</SubTitleOutgoing>
+              <SubTitleOutgoing>13 Jul</SubTitleOutgoing>
+            </ContainerSubTitleOutgoing>
+          </ContainerOutgoing>
+        </WrapperOutgoing>
+
+        <WrapperOutgoing>
+          <ContainerImage>
+            <Image source={car} />
+          </ContainerImage>
+
+          <ContainerOutgoing>
+            <ContainerTitleOutgoing>
+              <TitleOutgoing>Transporte</TitleOutgoing>
+              <TitleOutgoing>- R$ 12,55</TitleOutgoing>
+            </ContainerTitleOutgoing>
+
+            <ContainerSubTitleOutgoing>
+              <SubTitleOutgoing>UBER</SubTitleOutgoing>
+              <SubTitleOutgoing>02 Jul</SubTitleOutgoing>
+            </ContainerSubTitleOutgoing>
+          </ContainerOutgoing>
+        </WrapperOutgoing>
+      </SafeAreaContainer>
     </Container>
   );
 }
 
 export default Main;
+
+const style = StyleSheet.create({
+  shadow: {
+    shadowColor: '#fff',
+    elevation: 2,
+  },
+});
--- financialApp/src/components/Main/styles.js
+++ financialApp/src/components/Main/styles.js
@@ -15,7 +15,111 @@ export const ContainerTransactionsText = styled.View`
 `;
 
 export const TransactionsText = styled.Text`
-  font-size: ${(props) => (props.extraBold ? '20px' : '15px')};
-  color: ${(props) => (props.extraBold ? '#123D6A' : '#254C92')};
+  font-size: ${(props) => (props.extraBold ? '21px' : '17px')};
+  color: ${(props) => (props.extraBold ? '#081a59' : '#254C92')};
   font-weight: bold;
+`;
+
+export const ButtonsContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const WrapperButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.6,
+})`
+  background-color: #fff;
+  width: ${(props) => (props.noImage ? '75px' : '130px')};
+  margin-right: 7px;
+  align-items: center;
+  justify-content: center;
+  margin-left: ${(props) => (props.first ? '20px' : '0px')};
+  padding: 6px 0px;
+  border-radius: 20px;
+  /* border-top-left-radius: 20px;
+  border-top-right-radius: 20px; */
+  height: 40px;
+  flex-direction: row;
+  margin-bottom: 30px;
+`;
+
+export const Buttons = styled.Text`
+  font-size: 17px;
+  font-weight: bold;
+  color: #081a59;
+`;
+
+export const ButtonsIcons = styled.Image`
+  width: 25px;
+  height: 25px;
+  margin-right: 3px;
+  margin-left: -2px;
+`;
+
+export const SafeAreaContainer = styled.ScrollView.attrs({
+  vertical: true,
+})`
+  margin: 0 20px;
+`;
+
+export const TitleDays = styled.Text`
+  color: #b2bdcb;
+  font-size: 17px;
+  font-weight: bold;
+`;
+
+export const WrapperOutgoing = styled.View`
+  flex-direction: row;
+  background-color: #fff;
+  height: 100px;
+  margin-top: 20px;
+  align-items: center;
+  padding-left: 4px;
+  border-top-left-radius: ${(props) => (props.first ? '20px' : '0')};
+  border-top-right-radius: ${(props) => (props.first ? '20px' : '0')};
+  border-bottom-left-radius: ${(props) => (props.first ? '0' : '20px')};
+  border-bottom-right-radius: ${(props) => (props.first ? '0' : '20px')};
+  margin-bottom: ${(props) => (props.first ? '-18px' : '0')};
+`;
+
+export const ContainerImage = styled.View`
+  margin-right: 10px;
+  margin-left: 5px;
+  background-color: #f5f7f9;
+  align-self: center;
+  border-radius: 15px;
+  width: 60px;
+  height: 60px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Image = styled.Image`
+  width: 40px;
+  height: 40px;
+`;
+
+export const ContainerOutgoing = styled.View`
+  width: 260px;
+`;
+
+export const ContainerTitleOutgoing = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 6px;
+`;
+
+export const ContainerSubTitleOutgoing = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const TitleOutgoing = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #081a59;
+`;
+
+export const SubTitleOutgoing = styled.Text`
+  font-size: 17px;
+  color: #8090a2;
 `;
