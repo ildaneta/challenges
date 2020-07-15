c0699740c3f8112e1a878ba0af255e7de67a07e8
Ilda Neta <iildaemanoely@gmail.com>
2020-07-15 12:55:10 -0300

    feat: adding the menu in the app page

--- financialApp/src/App.js
+++ financialApp/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import Header from './components/Header/index';
 import Main from './components/Main/index';
+import Menu from './components/Menu/index';
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <Menu />
     </>
   );
 }
