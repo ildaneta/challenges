409bba50c1c1970d9b67aaf5cabec62dca1e0e3a
Ilda Neta <iildaemanoely@gmail.com>
2020-07-16 20:28:04 -0300

    feat: creating the home page

---
+++ financialApp/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../components/Header/index';
+import Main from '../components/Main/index';
+import Menu from '../components/Menu/index';
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Menu />
+    </>
+  );
+}
+
+export default Home;
