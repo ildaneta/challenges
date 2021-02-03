7bf77d3dcb8beb5f02a68a4196d86524a4586298
Ilda Neta <iildaemanoely@gmail.com>
2021-02-03 16:33:14 -0300

    feat: Criando a pagina Main e seu arquivo de estilos

---
+++ toDo-app/src/screens/main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Header from '../../components/header/Header';
+
+import { styles } from './styles';
+
+const Main = (): JSX.Element => {
+  return <Header headerTitle="ToDo Appp" />;
+};
+
+export default Main;
